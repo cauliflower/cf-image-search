2d6404d25fe8fc6470b7a1ab0d22e9e1a79f9689
Michal Budzynski <michal@virtualdesign.pl>
2014-03-28 21:09:00 +0100

    [cf-image-search] addon finished

--- data/image-listener.js
+++ data/image-listener.js
@@ -1,6 +1,17 @@
+var actionKeyCode = 83;
+var currentURL;
+
 document.body.addEventListener('click', function(event) {
   var element = event.target;
   if (element.nodeName.toUpperCase() === 'IMG') {
-    self.port.emit('elementsUrl', element.src);
+    console.log('Element added', element.src);
+    currentURL = element.src;
+  }
+});
+
+document.addEventListener('keyup', function(event) {
+  console.log('code', event.keyCode);
+  if (event.keyCode === actionKeyCode && currentURL !== undefined) {
+    self.port.emit('element-url', currentURL);
   }
 });
