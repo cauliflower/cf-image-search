75ba3b6ea760adf485ee0820cd61b92705d70587
Maciej Małecki <me@mmalecki.com>
2014-03-29 07:39:50 +0700

    Support `background-image`
    Fixes #1.

--- data/image-listener.js
+++ data/image-listener.js
@@ -3,9 +3,22 @@ var currentURL;
 
 document.body.addEventListener('click', function(event) {
   var element = event.target;
+  var urlRe = /url\("(.*)"\)/;
+  var computedBackgroundImage;
+  var match;
+
   if (element.nodeName.toUpperCase() === 'IMG') {
     currentURL = element.src;
   }
+  else {
+    computedBackgroundImage = window.getComputedStyle(element).getPropertyValue('background-image');
+    // According to https://developer.mozilla.org/en-US/docs/Web/CSS/background-image,
+    // computed property of `background-image` contains resolved URLs.
+    if (match = urlRe.exec(computedBackgroundImage)) {
+      currentURL = match[1];
+    }
+  }
+  console.log('Image found: ' + currentURL);
 });
 
 document.addEventListener('keyup', function(event) {
